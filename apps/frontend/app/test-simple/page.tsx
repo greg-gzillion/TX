dd4b5c8c032576f592aa27735413a631765e38f2
Greg <gjf20842@gmail.com>
2026-02-21 14:03:23 -0700

    Remove all test pages - clean production build

--- apps/frontend/app/test-simple/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function SimpleTest() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Simple Test Page</h1>
-      <p>If you can see this, React is working!</p>
-    </div>
-  );
-}
